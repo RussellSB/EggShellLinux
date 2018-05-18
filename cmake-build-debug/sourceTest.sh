6c70f3913febd733c4f7dd6940a47fad1ae20da5
russellsb <r.sammutbonnici@gmail.com>
2018-05-19 01:39:38 +0200

    documentation

---
+++ cmake-build-debug/sourceTest.sh
@@ -0,0 +1,14 @@
+print
+print Hey $USER! This source command seems to be working pretty well right?
+print You know what? I'm going to have some fun
+print First I'll print all your variables!
+all
+print Ooo, interesting "$CWD" I'll take you up to the root directory now
+chdir ..
+print it's now: $CWD
+print Great now that's happened, I'll set your "$PROMPT" to:
+PROMPT=INSANELY_LEGIT_PROMPT_:)_==>_
+print $PROMPT
+print If you'd like to reset the prompt use "chdir".
+print Russell made it so putting "chdir" on its own sets it back to the initial directory and it affects the prompt!
+print
